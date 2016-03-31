d088b1f1631db5c9d1c2509240ab3a1fa3b07395
Stephan14 <zsf657135507@gmail.com>
2016-03-31 09:49:34 +0800

    最终版答案待优化

--- questions/q127.cpp
+++ questions/q127.cpp
@@ -1,13 +1,74 @@
 #include<iostream>
+#include<string>
+#include<queue>
 #include<unordered_set>
+#include<map>
+
+using namespace std;
+
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+      queue<string> que;
+      map<string, int> map;
 
+      if( beginWord == "" || endWord == "" || wordList.empty() )
+        return 0;
+      if( beginWord == endWord )
+        return 1;
+      que.push( beginWord );
+      map.insert( pair<string, int>( beginWord, 1) );
+
+      while( !que.empty() )
+      {
+        string currentWord = que.front();
+        que.pop();
+        int currentLength = map[ currentWord ];
+        for( int index = 0; index < currentWord.length(); index++ )
+        {
+          for( char ch = 'a'; ch <= 'z'; ch++ )
+          {
+            string temp = currentWord;
+            if( ch == temp[ index ])
+              continue;
+            temp[ index ] = ch;
+            if( temp == endWord )
+              return currentLength+1;
+            if( !map.count( temp ) && wordList.count( temp ) )
+            {
+              que.push( temp );
+              //cout<<temp<<endl;
+              map.insert(pair<string, int>(temp, currentLength+1) );
+            }
+          }
+        }
+      }
+      return 0;
+    }
+    static int isDiffOneBit( string beginWord, string endWord )
+    {
+      int counter = 0, index ;
+
+      if( beginWord.length() != endWord.length() )
+        return 0;
+
+      for( index = 0; index < beginWord.length(); index++ )
+        if( beginWord[ index ] != endWord[ index ] && ++counter > 1 )
+          return 0;
+
+      return counter ;
     }
 };
 
 int main(int argc, char const *argv[]) {
-  
+  // if( Solution::isDiffOneBit( "adc", "abc") )
+  //   cout<<"一位不同"<<endl;
+  // else
+  //   cout<<"多维不同或者位数不同"<<endl;
+  Solution s;
+  unordered_set<string> set = {"kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"};
+
+
+  cout<<s.ladderLength( "cet" , "ism", set );
   return 0;
 }
