3e0113e88c30c1d39b909a985b69c8cc816a009e
Stephan14 <zsf657135507@gmail.com>
2016-04-02 23:19:53 +0800

    运行超时和带有缓存的深搜

--- questions/q62.cpp
+++ questions/q62.cpp
@@ -5,41 +5,56 @@ using namespace std;
 
 class Solution {
 public:
-  // int uniquePaths(int m, int n) {
-  //     vector< vector< int > > v( m, vector<int>(n, 0) );
-  //     return dfs( m-1, n-1, v);
-  // }
-  //
-  // int dfs( int x, int y, vector< vector<int> > &v)
-  // {
-  //   int num = 0;
-  //   if( x < 0 || y < 0 )
-  //     return 0;
-  //   if( x == 0 && y == 0 )
-  //     return 1;
-  //   return dfs( x, y-1, v ) + dfs( x-1, y, v );
-  // }
-
-    int uniquePaths(int m, int n){
-      f = vector< vector<int> >(m, vector<int>(n, 0) );
-      f[0][0] = 1;
-      return dfs(m-1,n-1);
-
+    int uniquePaths(int m, int n) {
+        vector< vector< int > > v( m, vector<int>(n, 0) );
+        int sum = 0;
+        sum = dfs( m-1, n-1, v);
+        return sum;
     }
-public:
-  vector< vector<int> > f;
-  int dfs( int x, int y )
-  {
-    //终止条件
-    if( x < 0 || y < 0 )
-      return 0;
-    if( x == 0 && y == 0 )
-      return f[x][y];
-    if( f[x][y] > 0 )
-      return f[x][y];
-    else
-      return f[x][y] = dfs(x-1, y) + dfs( x, y-1);
-  }
+
+    int dfs( int x, int y, vector< vector<int> > &v)
+    {
+      int num = 0;
+      if( x < 0 || y < 0 )
+        return 0;
+      if( x == 0 && y == 0 )
+        return 1;
+
+      if( x-1 >= 0 && v[x-1][y] == 0 )
+      {
+        v[x-1][y] = 1;
+        num += dfs( x-1, y, v );
+        v[x-1][y] = 0;
+      }
+      if( y-1 >= 0 && v[x][y-1] == 0 )
+      {
+        v[x][y-1] = 1;
+        num += dfs( x, y-1, v );
+        v[x][y-1] = 0;
+      }
+      return num;
+    }
+
+//     int uniquePaths(int m, int n){
+//       f = vector< vector<int> >(m, vector<int>(n, 0) );
+//       f[0][0] = 1;
+//       return dfs(m-1,n-1);
+//
+//     }
+// public:
+//   vector< vector<int> > f;
+//   int dfs( int x, int y )
+//   {
+//     //终止条件
+//     if( x < 0 || y < 0 )
+//       return 0;
+//     if( x == 0 && y == 0 )
+//       return f[x][y];
+//     if( f[x][y] > 0 )
+//       return f[x][y];
+//     else
+//       return f[x][y] = dfs(x-1, y) + dfs( x, y-1);
+//   }
 };
 
 
