166aee8a07ee4736313e18dbc0e1404150ab1db2
Stephan14 <zsf657135507@gmail.com>
2016-04-04 23:07:29 +0800

    部分代码

---
+++ questions/q53.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      int i, j, max = -100000;
+
+      for( i = 0; i < nums.size(); i++ )
+        for( j = i; j < mums.size(); j++ )
+          max = max < nums[j] ?
+
+    }
+
+    int findmax( int a, int b, int c )
+    {
+      
+    }
+};
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
