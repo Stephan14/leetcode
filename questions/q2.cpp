864359ad35c67b2b13605bb53cdb97e62bbe205b
Stephan14 <zsf657135507@gmail.com>
2016-04-24 22:27:26 +0800

    　最终版答案

--- questions/q2.cpp
+++ questions/q2.cpp
@@ -8,76 +8,84 @@
  * };
  */
 
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-    {
-      if( l1 == NULL || l2 == NULL )
-        return NULL;
+ class Solution {
+ public:
+     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+     {
+       if( l1 == NULL || l2 == NULL )
+         return NULL;
 
-      ListNode *reserverl1 = reserverList( l1 );
-      ListNode *reserverl2 = reserverList( l2 );
-      ListNode *top = NULL, *p = NULL;
-      int lastvalue = 0;
+       //ListNode *reserverl1 = reserverList( l1 );
+       //ListNode *reserverl2 = reserverList( l2 );
+       ListNode *top = NULL, *p = NULL;
+       int lastval = 0;
 
-      while( reserverl1 != NULL && reserverl2 != NULL )
-      {
-        int tempvalue = ( reserverl2->value + reserverl1->value + lastvalue ) % 10;
-        lastvalue = ( reserverl2->value + reserverl1->value + lastvalue ) / 10;
-        if( top == NULL )
-        {
-          top = new ListNode( 0 );
-          top->value = tempvalue;
-          p = top;
-        }
-        else
-        {
-          ListNode *temp = new ListNode( 0 );
-          temp->next = tempvalue;
-          p->next = temp;
-          p = p->next;
-        }
-        reserverl2 = reserverl2 -> next;
-        reserverl1 = reserverl1 -> next;
-      }
+       while( l1 != NULL && l2 != NULL )
+       {
+         int tempval = ( l2->val + l1->val + lastval ) % 10;
+         lastval = ( l2->val + l1->val + lastval ) / 10;
+         if( top == NULL )
+         {
+           top = new ListNode( 0 );
+           top->val = tempval;
+           p = top;
+         }
+         else
+         {
+           ListNode *temp = new ListNode( 0 );
+           temp->val = tempval;
+           p->next = temp;
+           p = p->next;
+         }
+         l2 = l2 -> next;
+         l1 = l1 -> next;
+       }
 
-      while( reserverl1 != NULL )
-      {
-        int tempvalue = ( reserverl1->value + lastvalue ) % 10;
-        lastvalue = ( reserverl1->value + lastvalue ) / 10;
-        ListNode *temp = new ListNode( 0 );
-        temp->next = tempvalue;
-        p->next = temp;
-        p = p->next;
-        reserverl1 = reserverl1 -> next;
-      }
+       while( l1 != NULL )
+       {
+         int tempval = ( l1->val + lastval ) % 10;
+         lastval = ( l1->val + lastval ) / 10;
+         ListNode *temp = new ListNode( 0 );
+         temp->val = tempval;
+         p->next = temp;
+         p = p->next;
+         l1 = l1 -> next;
+       }
 
-      while( reserverl2 != NULL )
-      {
-        int tempvalue = ( reserverl2->value + lastvalue ) % 10;
-        lastvalue = ( reserverl2->value + lastvalue ) / 10;
-        ListNode *temp = new ListNode( 0 );
-        temp->next = tempvalue;
-        p->next = temp;
-        p = p->next;
-        reserverl2 = reserverl2 -> next;
-      }
-      return reserverList( top );
-    }
+       while( l2 != NULL )
+       {
+         int tempval = ( l2->val + lastval ) % 10;
+         lastval = ( l2->val + lastval ) / 10;
+         ListNode *temp = new ListNode( 0 );
+         temp->val = tempval;
+         p->next = temp;
+         p = p->next;
+         l2 = l2 -> next;
+       }
 
-    ListNode　*reserverList(ListNode* l1)
-    {
-      ListNode* cur = NULL;
-      ListNode* last = NULL;
+       if( lastval != 0 )
+       {
+         ListNode *temp = new ListNode( 0 );
+         temp->val = lastval;
+         p->next = temp;
+         p = p->next;
+       }
+       return  top ;
+     }
 
-      while( l1 != NULL )
-      {
-        cur = l1;
-        l1 = l1->next;
-        cur->next = last;
-        last = cur;
-      }
+     ListNode  *reserverList(ListNode *l1)
+     {
+       ListNode* cur = NULL;
+       ListNode* last = NULL;
 
-      return cur;
-    }
-};
+       while( l1 != NULL )
+       {
+         cur = l1;
+         l1 = l1->next;
+         cur->next = last;
+         last = cur;
+       }
+
+       return cur;
+     }
+ };
