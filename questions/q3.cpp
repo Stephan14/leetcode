70c7cdc4de352eb4e2adde477d4e23e221dbffd1
stephan14 <zsf657135507@gmail.com>
2016-11-29 22:41:46 +0800

    添加注释

--- questions/q3.cpp
+++ questions/q3.cpp
@@ -8,14 +8,15 @@ public:
       int char_set[256];//保存上次字符出现的位置
       memset( char_set, -1, sizeof( char_set ) );
 
-      for( index = 0; index < s.size(); index++ )
-      {
+    for( index = 0; index < s.size(); index++ )
+    {
         if( char_set[ s[ index ] ] > cur_index )
-          cur_index = char_set[ s[ index ] ];
+        //字符重复出现
+            cur_index = char_set[ s[ index ] ];
         if( index - cur_index > max_lenghth )
           max_lenghth = index - cur_index;
         char_set[ s[ index ] ] = index;
-      }
+    }
       return max_lenghth;
     }
 };
