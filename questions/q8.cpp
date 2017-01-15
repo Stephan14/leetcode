57ae41168395b11977efc9d0b125c09f2862979b
stephan14 <zsf657135507@gmail.com>
2017-01-15 21:39:26 +0800

    问题8答案

--- questions/q8.cpp
+++ questions/q8.cpp
@@ -15,9 +15,12 @@ class Solution {
         long long cur=0;
         int num=0,i=0;
         int flag1=0,flag2=0;
+
         while(str[i]!='\0' && str[i]==' ') i++;//开头空格舍弃
+
         if(str[i]=='-') flag1++,i++;
         else if(str[i]=='+') flag2++,i++;
+
         for(; str[i]!='\0'; i++)
         {
             if(str[i]>='0' && str[i]<='9')
@@ -37,6 +40,7 @@ class Solution {
             }
             else break;
         }
+
         num=(int)cur;
         return num;
     }
