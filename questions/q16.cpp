d5843e02e7700444b35086c413f13635bd05464f
stephan14 <zsf657135507@gmail.com>
2017-02-14 10:24:31 +0800

    测试通过

--- questions/q16.cpp
+++ questions/q16.cpp
@@ -7,41 +7,33 @@
 
 #include<vector>
 #include<cmath>
+#include<algorithm>
 #include<iostream>
 using namespace std;
 
 class Solution {
     public:
     int threeSumClosest(vector<int>& nums, int target) {
-        vector< vector<int> > sum( nums.size(), vector<int>(nums.size(),0) );
-
-        int abs_number = INT_MAX;
-        int result = 0;
 
         if(nums.size() < 3)
-            return 0;
+        return 0;
+        sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < sum.size(); i++)
-            for(int j = i+1; j < sum.size(); j++)
-        {
-            sum[i][j] = nums[i] + nums[j];
-        }
+        int result = nums[0] + nums[1] + nums[2];
 
-        for(int index = 0; index < nums.size(); index++)
+        for(int index = 0; index < nums.size() - 2; index++)
         {
-            for(int j = 0; j < nums.size()-1; j++)
+            int start = index + 1;
+            int end =  nums.size() - 1;
+            while(start < end)
             {
-                if(index != j)
-                {
-                    for(int i = j + 1; i < nums.size(); i++)
-                    {
-                        if(i > index && abs(sum[j][i] + nums[index] - target) < abs_number)
-                        {
-                            abs_number = abs(sum[j][i] + nums[index] - target);
-                            result =  sum[j][i] + nums[index];
-                        }
-                    }
-                }
+                int sum = nums[index] + nums[start] + nums[end];
+                if(sum > target)
+                end--;
+                else
+                start++;
+                if(abs(sum - target) < abs(result - target))
+                result = sum;
             }
         }
         return result;
