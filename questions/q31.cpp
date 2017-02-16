de4f839340bc0210c8fbc657b24ddeeea0285b3b
stephan14 <zsf657135507@gmail.com>
2017-02-16 11:46:39 +0800

    全部测试用例通过

--- questions/q31.cpp
+++ questions/q31.cpp
@@ -11,30 +11,39 @@
 using namespace std;
 
 class Solution {
-public:
-    void swap(int &a,int &b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
-    }
+    public:
     void nextPermutation(vector<int>& nums) {
 
         if(nums.empty())
-           return;
+        return;
 
         int last = -1;
         for(int i = 0; i < nums.size()-1; i++)
         {
             if(nums[i] < nums[i+1])
-                last = i;    
+            last = i;    
         }
-    
+
         if(last == -1)
-            sort(nums.begin(),nums.end());
-        else
         {
-           swap(nums[last], nums[last+1]);
+            sort(nums.begin(),nums.end());
+            return;
+        }
+
+        int max = -1;
+        for(int i = nums.size()-1; i >= 0; i--)
+        {
+            if(nums[i] > nums[last])
+            {
+                max = i;
+                break;
+            } 
+        }
+
+        if(max != -1)
+        {
+            swap(nums[last], nums[max]);
+            reverse(nums.begin()+last+1, nums.end());
         }
     }
 };
