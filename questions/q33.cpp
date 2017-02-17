ba802d731194a2ea6edb107d29e1508f5569d1de
stephan14 <zsf657135507@gmail.com>
2017-02-17 10:27:12 +0800

    提高效率

--- questions/q33.cpp
+++ questions/q33.cpp
@@ -13,37 +13,30 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
-        if(nums.empty())
-            return -1;
-        //二分查找最小的元素的下标
-        int start = 0;
-        int end = nums.size()-1;
-        while(start < end)
-        {
-            int mid = (start + end) / 2;
-            if(nums[mid] > nums[end])
-                start = mid + 1;
-            else
-                end = mid;//不是mid - 1
-        }
-        cout << "二分查找: start : " << start << " end: " << end <<endl;
-        int smallest = start;
-        start = 0;
-        end = nums.size() - 1;
-        while(start <= end)
-        {
-            int mid = (start + end) / 2;
-            int realmid = (mid + smallest) % nums.size();//想像成一个环
-            cout << "mid : " << mid << " realmid : " << realmid << endl;
-            if(nums[realmid] == target)
-                return realmid;
-            else if(nums[realmid] < target)
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
 
+        if(nums.empty())
+        return -1;
+
+        int l = 0, r = nums.size()-1;
+        while(l<=r)
+        {
+            int m = (l+r)/2;
+            if(nums[m] == target) return m;
+            if(nums[m]>= nums[l])
+            {
+                if(nums[l]<=target && target< nums[m])
+                r=m-1;
+                else
+                l = m+1;
+            }
+            else
+            {
+                if(nums[m]< target && target<=nums[r])
+                l = m+1;
+                else
+                r = m-1;
+            }
+        }
         return -1;
     }
 };
