bfc3257da1cc747bf76e4bdc127af486585d09db
stephan14 <zsf657135507@gmail.com>
2017-02-26 17:49:26 +0800

    不重复使用同一个元素

---
+++ questions/q40.cpp
@@ -0,0 +1,53 @@
+/*************************************************************************
+	> File Name: q40.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 日  2/26 16:44:16 2017
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int> > result;
+        vector<int> com;
+
+        sort(candidates.begin(), candidates.end()); //排序去重
+        combinationSum(result, com, candidates, target, 0); 
+
+        return result;
+    }
+private:
+
+    void combinationSum(vector<vector<int> > &res, vector<int> &combination, vector<int> &candidates, int target, int begin)
+    {
+        if( !target ) 
+        {
+            res.push_back( combination );
+            return;
+        }
+        
+        for(int i = begin; i < candidates.size() - 1 && target >= candidates[i]; i++)
+        {
+            if(i == begin || candidates[i] != candidates[i - 1]) //去重
+            {
+                combination.push_back( candidates[i]);
+                combinationSum(res, combination, candidates, target - candidates[i], i + 1);
+                combination.pop_back();
+            }
+        }
+
+        return;
+    }
+
+};
+
+int main()
+{
+    return 0;
+}
