340636dd27c3146b12013502105814953a2a1cc3
stephan14 <zsf657135507@gmail.com>
2017-02-27 22:17:20 +0800

    全部通过，修改if判断语句

--- questions/q41.cpp
+++ questions/q41.cpp
@@ -13,21 +13,29 @@ class Solution {
 public:
     //类似于计数排序
     int firstMissingPositive(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(nums[i] > 0 && nums[i] <= nums.size())
-            {
-                swap(nums[i], nums[nums[i] - 1]);
-            } 
-        }
+        int n = nums.size();
 
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(nums[i] != i + 1)
-                return i + 1;
-        }
+        for (int i=0; i<n; i++)  
+        {  
+            if (nums[i] > 0 && nums[i] < n)  
+            {  
+                //if (nums[i]-1 != i && nums[nums[i]-1] != nums[i])不用那么多条件就可以了。  
+                //因为只要是已经到位了的元素即：nums[i]-1==i了，那么判断如果有重复元素  
+                //两个位置交换就最好考虑好两个位置出现的可能情况。考虑问题全面，两个条件都考虑好。  
+                //update:增加i!=nums[i]表示i位置没到位，nums[nums[i]-1] != nums[i]表示nums[i]-1位置没到位，两个位置都判断也很好的。  
+                if (nums[nums[i]-1] != nums[i])  
+                {  
+                    swap(nums[nums[i]-1], nums[i]);  
+                    i--;  
+                }  
+            }  
+        }  
 
-        return nums.size() + 1;
+        for (int j=0; j<n; ++j)  
+        if (nums[j]-1 != j)  
+        return j+1;  
+
+        return n+1; 
     }
 };
 
