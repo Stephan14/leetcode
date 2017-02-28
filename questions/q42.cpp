e411220ed0240f726bb6e7b7bffb0ebf10af8204
stephan14 <zsf657135507@gmail.com>
2017-02-28 23:01:55 +0800

    通过测试，但是效率不高

---
+++ questions/q42.cpp
@@ -0,0 +1,48 @@
+/*************************************************************************
+	> File Name: q42.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 二  2/28 22:13:09 2017
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+     
+        int left = 0, right = height.size() - 1;
+        int maxleft = 0, maxright = 0;
+        int result = 0;
+
+        while(left < right)
+        {
+            if(height[left] <= height[right]) 
+            {
+                if(height[left] > maxleft )
+                    maxleft = height[left];
+                else
+                    result += maxleft - height[left];
+                left++;
+            }
+            else
+            {
+                if(height[right] > maxright) 
+                    maxright = height[right];
+                else
+                    result += maxright - height[right];
+                right--;
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
