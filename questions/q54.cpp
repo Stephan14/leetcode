b3af51b7da644c6e7e4028f0e87c5e8339f6b034
stephan14 <zsf657135507@gmail.com>
2017-03-06 23:19:40 +0800

    测试通过，效率低

--- questions/q54.cpp
+++ questions/q54.cpp
@@ -13,42 +13,30 @@ using namespace std;
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int> >& matrix) {
-        int rowBegin = 0;
-        int rowEnd = matrix.size() - 1;
-        int colBegin = 0;
-        int colEnd = matrix[0].size() - 1;
         vector<int> res;
 
-        if (matrix.empty() || matrix[0].empty()) return res;
+        if (matrix.empty() || matrix[0].empty())
+            return res;
 
-        while(rowBegin <= rowEnd && colBegin <= colEnd ) 
-        {
-            for(int i = colBegin; i <= colEnd; i++)
-            {
-                res.push_back(matrix[rowBegin][i]);
-            }
-            rowBegin++;
-            
-            for(int i = rowBegin; i <= rowEnd; i++)
-            {
-                res.push_back(matrix[i][colEnd]);  
-            }
-            colEnd--;
+        int m = matrix.size(), n = matrix[0].size();
+        int c = m > n ? (n + 1) / 2 : (m + 1) / 2;
+        int p = m, q = n;
 
-            for(int i = colEnd; i >= colBegin; i--)
-            {
-                res.push_back(matrix[rowEnd][i]);
-            }
-            rowEnd--;
-
-            for(int i = rowEnd; i >= rowBegin; i--)
-            {
-                res.push_back(matrix[i][colBegin]);
-            }
-            colBegin++;
+        for (int i = 0; i < c; ++i, p -= 2, q -= 2) {
+            for (int col = i; col < i + q; ++col)
+                res.push_back(matrix[i][col]);
+            for (int row = i + 1; row < i + p; ++row)
+                res.push_back(matrix[row][i + q - 1]);
+            if (p == 1 || q == 1) break;
+            for (int col = i + q - 2; col >= i; --col)
+                res.push_back(matrix[i + p - 1][col]);
+            for (int row = i + p - 2; row > i; --row)
+                res.push_back(matrix[row][i]);
         }
+
         return res;
     }
+
 };
 
 
