31168acc9c11ce6177ba4bad22a7fc8c4784e3e9
stephan14 <zsf657135507@gmail.com>
2017-03-17 21:33:26 +0800

    效率极高

--- questions/q78.cpp
+++ questions/q78.cpp
@@ -14,29 +14,20 @@ class Solution {
 public:
     //利用递归解决问题
     vector<vector<int> > subsets(vector<int>& nums) {
-        vector<vector<int> > result;
-        vector<int> out;
-
-        if(nums.empty())   
-        {
-            result.push_back(nums);  
-            return result;
-        }
+        vector<vector<int> > result(1);//初始化空集
 
         sort(nums.begin(), nums.end());
-        getSubsets(nums, 0, out, result);
-        return result;
-    }
-private:
-    void getSubsets(vector<int>nums, int index, vector<int> &out, vector<vector<int> > &res)
-    {
-        res.push_back(out);    
-        for(int i = index; i < nums.size(); i++)
+        for(int i = 0; i < nums.size(); i++)
         {
-            out.push_back(nums[i]);
-            getSubsets(nums, i+1, out, res);
-            out.pop_back();
+            int size = result.size();
+            for(int j = 0; j < size; j++)
+            {
+                result.push_back(result[j]);
+                result.back().push_back(nums[i]);
+            }
         }
+        
+        return result;
     }
 };
 
