9e1d0fd0eb384969b59f8a9ae97edbaf3e1d8764
stephan14 <zsf657135507@gmail.com>
2017-04-26 10:24:10 +0800

    使用unordered_map<pair<int, int>, int> 数据结构，没有直接结算斜率

--- questions/q149.cpp
+++ questions/q149.cpp
@@ -1,8 +1,8 @@
 /*************************************************************************
-       > File Name: q149.cpp
-       > Author:
-       > Mail:
-       > Created Time: 二  4/25 10:24:57 2017
+> File Name: q149.cpp
+> Author:
+> Mail:
+> Created Time: 二  4/25 10:24:57 2017
 ************************************************************************/
 
 #include <iostream>
@@ -11,44 +11,46 @@
 
 using namespace std;
 /**
- * Definition for a point.
- */
+* Definition for a point.
+*/
 struct Point {
-  int x;
-  int y;
-  Point() : x(0), y(0) {}
-  Point(int a, int b) : x(a), y(b) {}
+    int x;
+    int y;
+    Point() : x(0), y(0) {}
+    Point(int a, int b) : x(a), y(b) {}
 };
 
 class Solution {
-public:
-  int maxPoints(vector<Point> &points) {
-    if (points.size() < 3)
-      return points.size();
-    int result = 0;
+    public:
+    int maxPoints(vector<Point> &points) {
+        if (points.size() < 3)
+        return points.size();
+        int result = 0;
 
-    for (int i = 0; i < points.size(); i++) {
-      int samplePoint = 1;
-      unordered_map<double, int> slopesMap; //存储斜率以及出现的次数
-      for (int j = i + 1; j < points.size(); j++) {
-        if (points[i].x == points[j].x && points[j].y == points[i].y)
-          samplePoint++;
-        else if (points[i].x == points[j].x)
-          slopesMap[INT_MAX]++;
-        else {
-          double slopes =
-              double(points[i].y - points[j].y) / double(points[i].x - points[j].x);
-          slopesMap[slopes]++;
+        for (int i = 0; i < points.size(); i++) {
+            int samplePoint = 1;
+            unordered_map<pair<int, int>, int> slopesMap; //存储斜率以及出现的次数
+
+            for (int j = i + 1; j < points.size(); j++) {
+                if (points[i].x == points[j].x && points[j].y == points[i].y)
+                samplePoint++;
+                int dy = points[i].y - points[j].y;
+                int dx = points[i].x - points[j].x;
+                int d = gcd(dy, dx);
+                slopesMap[{dy / d, dx / d}]++;
+            }
+            //遍历map找出出现频率最高的斜率
+            int result = max(result, samplePoint);
+            for (auto it = slopesMap.begin(); it != slopesMap.end(); it++)
+            result = max(result, it->second + samplePoint);
         }
-      }
-      //遍历map找出出现频率最高的斜率
-      int maxTimes = 0;
-      for (auto it = slopesMap.begin(); it != slopesMap.end(); it++)
-        maxTimes = max(maxTimes, it->second);
 
-      result = max(result, maxTimes + samplePoint);
+        return result;
     }
 
-    return result;
-  }
+    private:
+    int gcd(int a, int b)
+    {
+        return b == 0 ? a : gcd(b, a % b);
+    }
 };
