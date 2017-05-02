8542d0493746906ed7ce12a577caf21ed8f29be9
stephan14 <zsf657135507@gmail.com>
2017-05-02 10:23:34 +0800

    一对一映射

--- questions/q205.cpp
+++ questions/q205.cpp
@@ -14,25 +14,12 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         
-        if(s.size() != t.size())
-            return false;
-
-        int i = 0;
-        vector<char> charArray(256, ' ');
-        while(i < s.size())
-        {
-            if(charArray[s[i]] == ' ' && charArray[t[i]] == ' ')
-            {
-                charArray[s[i]] = t[i];
-                charArray[t[i]] = s[i];
-            }
-            else if((charArray[s[i]] != ' ' && charArray[s[i]] != t[i]) ||
-                    (charArray[t[i]] != ' ' && charArray[t[i]] != s[i]))
-                    return false;
-
-            i++;
+        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
         }
-
         return true;
     }
 };
