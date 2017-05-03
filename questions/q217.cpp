83df9944743f482b53e9bc2a391a765bafd67c4c
stephan14 <zsf657135507@gmail.com>
2017-05-03 13:52:53 +0800

    利用set 重复插入无效

--- questions/q217.cpp
+++ questions/q217.cpp
@@ -6,29 +6,13 @@
  ************************************************************************/
 
 #include<iostream>
-#include<map>
-#include<vector>
-
+#include<set>
 
 using namespace std;
 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        if(nums.size() < 2)
-            return false;
-
-        map<int, int> numTimesMap;
-        for(auto &num : nums)
-        {
-            if(numTimesMap.find(num) != numTimesMap.end())
-                numTimesMap[num]++;
-            else
-                numTimesMap[num] = 1;
-            if(numTimesMap[num] > 1)
-                return true;
-        }
-
-        return false;
+        return nums.size() > set<int>(nums.begin(), nums.end()).size();
     }
 };
