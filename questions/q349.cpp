79e7a8071a7ca19223ef70dce8ca33d269ec22ba
stephan14 <zsf657135507@gmail.com>
2017-06-02 23:31:14 +0800

    使用双指针

--- questions/q349.cpp
+++ questions/q349.cpp
@@ -22,4 +22,28 @@ public:
         }
         return vector<int>(result.begin(), result.end());
     }
+    //使用双指针
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int nums1Index = 0, nums2Index = 0;
+        vector<int> result;
+        
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        while(nums1Index < nums1.size() && nums2Index < nums2.size())
+        {
+            if(nums1[nums1Index] < nums2[nums2Index])
+                nums1Index++;
+            else if(nums1[nums1Index] > nums2[nums2Index])
+                nums2Index++;
+            else 
+            {
+                if(result.empty() || result.back() != nums1[nums1Index])
+                    result.push_back(nums1[nums1Index]);
+                nums1Index++, nums2Index++;
+            }
+        }
+
+        return result;
+    }    
 };
