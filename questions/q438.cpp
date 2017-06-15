ee54cfb4bbc218134c8f8a7154a99e96e2c7d21f
stephan14 <zsf657135507@gmail.com>
2017-06-15 20:48:26 +0800

    使用滑动窗口

--- questions/q438.cpp
+++ questions/q438.cpp
@@ -14,30 +14,30 @@ using namespace std;
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        
-        //使用两个数组来比较字符是否出现的一样
-        //每个数组记录一段区域中字符出现的位置
+        //使用两个指针固定一个窗口，判断窗口中的字符统计的值
+        //与目标字符串字符统计的值是否相同
         vector<int> result;
-        vector<int> sMap(128, 0), pMap(128, 0);
-        if(s.empty())
-            return {};//返回空数组
+        int left = 0, right = 0, total = p.size();
+        vector<int> charMap(256, 0);
+    
+        for(char ch : s)
+            charMap[ch]++;
 
-        for(int i = 0; i < p.size(); i++)
+        while(right < s.size())
         {
-            ++sMap[s[i]];
-            ++pMap[p[i]];
+            //每次都移动右指针，当如果字符存在于p的字符串中，降低数量total
+            //如果当前字符的数量大于等于1，则存在
+            if(charMap[s[right++]]-- >= 1)
+                --total;
+            //找打符合目标的字符串
+            if(total == 0)
+                result.push_back(left);
+            //字符长度等于目标字符长度，则向左移动指针
+            //并重置map的字符数值
+            if(right - left == p.size() && charMap[left++]++ >= 0)  
+                ++total;
         }
-        
-        if(sMap == pMap)
-            result.push_back(0);
-        for(int i = p.size(); i < s.size(); i++)
-        {
-            ++sMap[s[i]];
-            --sMap[s[i - p.size()]];
-            if(sMap == pMap)
-                result.push_back(i - p.size() + 1);
-        }
-        
+
         return result;
     }
 
