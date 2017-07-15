759d70a51018d65e4e454623555e0dbf380f7af7
stephan14 <zsf657135507@gmail.com>
2017-07-15 19:24:15 +0800

    存储中间结果

--- questions/q70.cpp
+++ questions/q70.cpp
@@ -6,18 +6,46 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+
 using namespace std;
 class Solution {
-public:
+    public:
     int climbStairs(int n) {
-        
-        if(n == 1)
-            return 1;
-        else if(n == 2)
-            return 2;
-        else
-            return climbStairs(n-1) + climbStairs(n-2); 
+
+        int* p = new int[n+1];
+        p[1] = 1;
+        p[2] = 2;
+
+        int record = n;
+        return climbStairs(n, record, p);
+
 
     }
 
+    int climbStairs(int n, int& record, int array[])
+    {
+        if(n == 1)
+        {
+            record = 1;
+            return array[1];
+
+        }
+        else if(n == 2)
+        {
+            record = 2;
+            return array[2];
+
+        }
+        else if(n > record)
+        return array[n];
+        else
+        {
+            record = n - 1;
+            array[n] = climbStairs(n-1, record, array) + climbStairs(n-2, record, array);
+            return array[n];
+
+        }
+
+    }
 };
